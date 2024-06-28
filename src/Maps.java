83b7d33293dab8233722dfec49958631e88ecb0c
chani <chan.chanikya23@gmail.com>
2024-06-28 11:41:51 -0500

    name changed to namee

--- src/Maps.java
+++ src/Maps.java
@@ -52,7 +52,7 @@ class Employee{
     }
     @Override
     public String toString() {
-        return "[name=" + name + ", address=" + address + ", role=" + role + "]";
+        return "[namee=" + name + ", address=" + address + ", role=" + role + "]";
     }
 
 
